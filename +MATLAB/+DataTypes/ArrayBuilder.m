008245d676cd394bfa32489ceabdd9d78fe71c20
埃博拉酱 <vhtmfu@outlook.com>
2022-02-22 20:43:58 +0800

    修复ArrayBuilder的Storage默认是double而不是cell的问题

--- +MATLAB/+DataTypes/ArrayBuilder.m
+++ +MATLAB/+DataTypes/ArrayBuilder.m
@@ -3,7 +3,7 @@ classdef ArrayBuilder<handle
 	%将数据向内存中积累时，经常遇到需要不断累加数组的问题，MATLAB会提示预分配内存。但如果读取之前无法得知将会有多少项，预分配就会变成一个十分麻烦的动态任务。本类建立一个增长维度，在此维度上可以不断累加一个内置的数组。用户只需不断Append即可，无需考虑内存分配的问题，本类会自动进行优化的内存管理。
 	properties(Access=private)
 		%数据存储实体
-		Storage
+		Storage cell
 		Capacity=0
 		%当前在累加维度上的累加数
 		Stock=0
--- +MATLAB/Version.m
+++ +MATLAB/Version.m
@@ -1,4 +1,4 @@
 function V = Version
-V.Me='7.0.1';
+V.Me='7.0.2';
 V.Win32API=Win32API.Version;
 V.MATLAB='R2022a';
