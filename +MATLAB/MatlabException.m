2b812a3ca4bcb70803924e66194e4d218a580d4d
埃博拉酱 <vhtmfu@outlook.com>
2022-08-19 11:14:14 +0800

    DataTypes.Select：找不到指定字段时输出更详细信息

--- +MATLAB/MatlabException.m
+++ +MATLAB/MatlabException.m
@@ -4,6 +4,8 @@ classdef MatlabException<uint8&MATLAB.IEnumerableException
 		Input_is_not_a_string(1)
 		File_operation_failed(2)
 
+		Missing_matching_key(251)
+		Fields_not_found_in_tables(252)
 		Error_copying_file(253)
 		Error_deleting_file(254)
 		Error_moving_file(255)
--- +MATLAB/Version.m
+++ +MATLAB/Version.m
@@ -1,3 +1,3 @@
 function V = Version
-V.Me='9.0.1';
+V.Me='9.0.2';
 V.MATLAB='R2022a';
