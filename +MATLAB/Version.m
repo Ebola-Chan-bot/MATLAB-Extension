798389b727d455819beea3eb721afe98b418f045
vhtmf <vhtmf@BOX12>
2023-02-25 10:39:00 +0800

    SupportPkg.CheckUpdateFromGithub

--- +MATLAB/Version.m
+++ +MATLAB/Version.m
@@ -1,3 +1,7 @@
 function V = Version
-V.Me='10.3.0';
+V.Me='v10.3.0';
 V.MATLAB='R2021a';
+persistent NewVersion
+if isempty(NewVersion)
+	NewVersion=MATLAB.SupportPkg.CheckUpdateFromGithub('https://github.com/Silver-Fang/MATLAB-Extension/releases','埃博拉酱的MATLAB扩展',V.Me);
+end
