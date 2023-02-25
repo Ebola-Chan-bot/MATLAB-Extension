967573802d9828e556b1d05acfdb7aeb0b29025a
埃博拉酱 <vhtmfu@outlook.com>
2023-02-25 11:58:10 +0800

    如果安装了文本分析工具箱，可以自动检查更新

--- +MATLAB/Version.m
+++ +MATLAB/Version.m
@@ -2,6 +2,12 @@ function V = Version
 V.Me='v10.3.0';
 V.MATLAB='R2021a';
 persistent NewVersion
-if isempty(NewVersion)
-	NewVersion=MATLAB.SupportPkg.CheckUpdateFromGithub('https://github.com/Silver-Fang/MATLAB-Extension/releases','埃博拉酱的MATLAB扩展',V.Me);
+try
+	if isempty(NewVersion)
+		NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/MATLAB-Extension/releases','埃博拉酱的MATLAB扩展',V.Me);
+	end
+catch ME
+	if ME.identifier~="MATLAB:undefinedVarOrClass"
+		ME.rethrow;
+	end
 end
