29df751f5bc388678f9b543490dea09340b575c0
埃博拉酱 <vhtmfu@outlook.com>
2024-08-11 15:45:05 +0800

    Ops.Pipeline：支持输出到工作区变量

--- +MATLAB/+Ops/Pipeline.m
+++ +MATLAB/+Ops/Pipeline.m
@@ -11,6 +11,9 @@ classdef Pipeline
 	%
 	% %不返回运算结果，而是将其交给某个终端消费函数
 	% Value|MATLAB.Ops.Pipeline|Operation1|Operation2|Operation3|…|OperationEnd;
+	%
+	% %将运算结果设为某个当前工作区的变量名
+	% Value|MATLAB.Ops.Pipeline|Operation1|Operation2|Operation3|…|VariableName;
 	% ```
 	%% 示例
 	% ```
@@ -20,7 +23,10 @@ classdef Pipeline
 	% %求随机4阶方阵的总和
 	%
 	% 4|MATLAB.Ops.Pipeline|@rand|{@sum,'all'}|@disp
-	% %求随机4阶方阵的总和并显示在命令行窗口中
+	% %求随机4阶方阵的总和并显示在命令行窗口中}|{}
+	%
+	% 4|MATLAB.Ops.Pipeline|@rand|{@sum,'all'}|'RandSum'
+	% %求随机4阶方阵的总和并赋值给变量RandSum
 	% ```
 	%% 输入参数
 	% Value，管线起始值
@@ -28,6 +34,7 @@ classdef Pipeline
 	% - function_handle，此函数句柄必须支持仅输入一个参数的语法，将把上一个管线操作的唯一输出值作为此函数句柄的输入参数。
 	% - (1,:)cell，此元胞数组的首个元素必须是function_handle，此函数句柄必须接受上一个管线操作的唯一输出值作为第1个输入参数，接受元胞数组的后续其它元素依次作为后续输
 	%  入参数。特别地，OperationEnd可以为空，此时直接返回计算结果值
+	% VariableName(1,1)string，要赋值的变量名。可以放在管线末端，输出值将被赋给具有该名称的工作区变量。这个工作区是调用管线运算符的工作区。
 	%% 返回值
 	% PipeEnd，包装最后一个管线操作（空元胞之前的最后一个）的唯一输出值。如果不获取输出值，则管线操作序列中的最后一个操作函数句柄可以不返回值。
 	properties(SetAccess=immutable)
@@ -48,6 +55,8 @@ classdef Pipeline
 				varargout={obj.Value};
 			elseif isa(Operation,'MATLAB.Ops.Pipeline')
 				varargout={Pipeline(obj)};
+			elseif isvarname(Operation)
+				assignin('caller',Operation,obj.Value);
 			else
 				try
 					if iscell(Operation)
