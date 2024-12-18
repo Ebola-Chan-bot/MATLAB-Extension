bc389726bd6681a975bc937fbc095c4afc2e2bd7
埃博拉酱 <vhtmfu@outlook.com>
2024-12-18 15:11:52 +0800

    更新Version检查URL

--- +MATLAB/Version.m
+++ +MATLAB/Version.m
@@ -4,7 +4,7 @@ V.MATLAB='R2024b';
 persistent NewVersion
 try
 	if isempty(NewVersion)
-		NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/MATLAB-Extension/releases','埃博拉酱的MATLAB扩展',V.Me);
+		NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Ebola-Chan-bot/MATLAB-Extension/releases','埃博拉酱的MATLAB扩展',V.Me);
 	end
 catch ME
 	if ME.identifier~="MATLAB:undefinedVarOrClass"
