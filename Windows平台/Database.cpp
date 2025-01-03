a8f3e00e105f179af6ac2c023a3b88e2c37e97eb
埃博拉酱 <vhtmfu@outlook.com>
2025-01-03 17:35:27 +0800

    20250103

--- Windows平台/Database.cpp
+++ Windows平台/Database.cpp
@@ -14,10 +14,10 @@ Mex工具API(Database_MariaDB)
 		URL = 万能转码<std::string>(std::move(输入[1]));
 		break;
 	case 5:
-		URL = (std::ostringstream("jdbc:mariadb://") << 万能转码<std::string>(std::move(输入[1])) << ":3306/" << 万能转码<std::string>(std::move(输入[2])) << "?user=" << 万能转码<std::string>(std::move(输入[3])) << "&password=" << 万能转码<std::string>(std::move(输入[4]))).str();
+		URL = (std::ostringstream("jdbc:mariadb://",std::ios::app) << 万能转码<std::string>(std::move(输入[1])) << ":3306/" << 万能转码<std::string>(std::move(输入[2])) << "?user=" << 万能转码<std::string>(std::move(输入[3])) << "&password=" << 万能转码<std::string>(std::move(输入[4]))).str();
 		break;
 	case 6:
-		URL = (std::ostringstream("jdbc:mariadb://") << 万能转码<std::string>(std::move(输入[1])) << ":" << 万能转码<uint16_t>(std::move(输入[2])) << "/" << 万能转码<std::string>(std::move(输入[3])) << "?user=" << 万能转码<std::string>(std::move(输入[4])) << "&password=" << 万能转码<std::string>(std::move(输入[5]))).str();
+		URL = (std::ostringstream("jdbc:mariadb://", std::ios::app) << 万能转码<std::string>(std::move(输入[1])) << ":" << 万能转码<uint16_t>(std::move(输入[2])) << "/" << 万能转码<std::string>(std::move(输入[3])) << "?user=" << 万能转码<std::string>(std::move(输入[4])) << "&password=" << 万能转码<std::string>(std::move(输入[5]))).str();
 		break;
 	default:
 		EnumThrow(MATLAB::Exception::The_number_of_entered_parameters_is_incorrect);
@@ -142,7 +142,8 @@ Mex工具API(Database_UpdateByPrimary)
 {
 	sql::Connection* const 连接 = 万能转码<sql::Connection*>(std::move(输入[1]));
 	const std::string 表名 = 万能转码<std::string>(std::move(输入[2]));
-	const Reference<Struct>更新表 = StructArray{ std::move(输入[3]) } [0] ;
+	const StructArray 结构体数组{ std::move(输入[3]) };//必须保留此对象，否则Struct会失效
+	const Struct 更新表 = 结构体数组[0];
 	const CellArray 所有列值{ 更新表["ColumnValue"] };
 	const size_t 列数 = 所有列值.getNumberOfElements();
 	const std::unique_ptr<std::string[]>所有列名 = std::make_unique_for_overwrite<std::string[]>(列数);
@@ -156,83 +157,13 @@ Mex工具API(Database_UpdateByPrimary)
 		适配器数组[列] = std::unique_ptr<通用列适配器>{ apply_visitor(std::move(所有列值[列]),适配访问器) };
 	}
 	const std::unique_ptr<sql::Statement> 语句(连接->createStatement());
-	std::ostringstream 语句文本("SELECT COUNT(*)FROM ");
+	std::ostringstream 语句文本("SELECT COUNT(*)FROM ", std::ios::app);
 	语句文本 << 表名 << " LIMIT 1";
-	const std::unique_ptr<sql::ResultSet> 结果集(语句->executeQuery(语句文本.str()));
-	结果集->next();
-	if (结果集->getInt(1))
+	try
 	{
-		语句文本.str("ALTER TABLE ");
-		语句文本 << 表名 << " ADD IF NOT EXISTS";
-		std::ostringstream 列定义流('(');
-		if (列数)
-			for (列 = 0;;)
-			{
-				列定义流 << 所有列名[列] << ' ' << 所有列类型[列] << " NULL";
-				if (++列 >= 列数)
-					break;
-				列定义流 << ',';
-			}
-		列定义流 << ')';
-		std::string 列定义文本 = 列定义流.str();
-		语句文本 << 列定义文本;
-		语句->addBatch(语句文本.str());
-		语句文本.str("CREATE OR REPLACE TEMPORARY TABLE _MATLAB_临时表");
-		语句文本 << 列定义文本;
-		语句->addBatch(语句文本.str());
-		语句->executeBatch();
-		语句文本.str("INSERT _MATLAB_临时表(");
-		列定义流.str("");
-		if (列数)
-			for (列 = 0;;)
-			{
-				列定义流 << 所有列名[列];
-				if (++列 >= 列数)
-					break;
-				列定义流 << ',';
-			}
-		列定义文本 = 列定义流.str();
-		语句文本 << 列定义文本 << ")VALUES";
-		const size_t 行数 = 所有列值[0].getNumberOfElements();
-		size_t 行;
-		if (行数 && 列数)
-			for (行 = 0;;)
-			{
-				语句文本 << '(';
-				for (列 = 0;;)
-				{
-					语句文本 << '?';
-					if (++列 >= 列数)
-						break;
-					语句文本 << ',';
-				}
-				语句文本 << ')';
-				if (++行 >= 行数)
-					break;
-				语句文本 << ',';
-			}
-		const std::unique_ptr<sql::PreparedStatement> 准备好的语句{ 连接->prepareStatement(语句文本.str()) };
-		int32_t 参数序号 = 1;
-		if (行数 && 列数)
-		{
-			for (行 = 0; 行 < 行数; ++行)
-				for (列 = 0; 列 < 列数; ++列)
-					适配器数组[列]->插入语句(准备好的语句.get(), 参数序号++);
-			准备好的语句->execute();
-		}
-		语句文本.str("INSERT ");
-		语句文本 << 表名 << '(' << 列定义文本 << ")SELECT " << 列定义文本 << " FROM _MATLAB_临时表 ON DUPLICATE KEY UPDATE ";
-		if (列数)
-			for (列 = 0;;)
-			{
-				语句文本 << 表名 << '.' << 所有列名[列] << "=_MATLAB_临时表." << 所有列名[列];
-				if (++列 >= 列数)
-					break;
-				语句文本 << ',';
-			}
-		语句->execute(语句文本.str());
+		语句->executeQuery(语句文本.str());
 	}
-	else
+	catch (const std::exception&)
 	{
 		语句文本.str("CREATE TABLE ");
 		语句文本 << 表名 << '(';
@@ -284,5 +215,75 @@ Mex工具API(Database_UpdateByPrimary)
 					适配器数组[列]->插入语句(准备好的语句.get(), 参数序号++);
 			准备好的语句->execute();
 		}
+		return;
 	}
+	语句文本.str("ALTER TABLE ");
+	语句文本 << 表名 << " ADD IF NOT EXISTS";
+	std::ostringstream 列定义流("(", std::ios::app);
+	if (列数)
+		for (列 = 0;;)
+		{
+			列定义流 << 所有列名[列] << ' ' << 所有列类型[列] << " NULL";
+			if (++列 >= 列数)
+				break;
+			列定义流 << ',';
+		}
+	列定义流 << ')';
+	std::string 列定义文本 = 列定义流.str();
+	语句文本 << 列定义文本;
+	语句->addBatch(语句文本.str());
+	语句文本.str("CREATE OR REPLACE TEMPORARY TABLE _MATLAB_临时表");
+	语句文本 << 列定义文本;
+	语句->addBatch(语句文本.str());
+	语句->executeBatch();
+	语句文本.str("INSERT _MATLAB_临时表(");
+	列定义流.str("");
+	if (列数)
+		for (列 = 0;;)
+		{
+			列定义流 << 所有列名[列];
+			if (++列 >= 列数)
+				break;
+			列定义流 << ',';
+		}
+	列定义文本 = 列定义流.str();
+	语句文本 << 列定义文本 << ")VALUES";
+	const size_t 行数 = 所有列值[0].getNumberOfElements();
+	size_t 行;
+	if (行数 && 列数)
+		for (行 = 0;;)
+		{
+			语句文本 << '(';
+			for (列 = 0;;)
+			{
+				语句文本 << '?';
+				if (++列 >= 列数)
+					break;
+				语句文本 << ',';
+			}
+			语句文本 << ')';
+			if (++行 >= 行数)
+				break;
+			语句文本 << ',';
+		}
+	const std::unique_ptr<sql::PreparedStatement> 准备好的语句{ 连接->prepareStatement(语句文本.str()) };
+	int32_t 参数序号 = 1;
+	if (行数 && 列数)
+	{
+		for (行 = 0; 行 < 行数; ++行)
+			for (列 = 0; 列 < 列数; ++列)
+				适配器数组[列]->插入语句(准备好的语句.get(), 参数序号++);
+		准备好的语句->execute();
+	}
+	语句文本.str("INSERT ");
+	语句文本 << 表名 << '(' << 列定义文本 << ")SELECT " << 列定义文本 << " FROM _MATLAB_临时表 ON DUPLICATE KEY UPDATE ";
+	if (列数)
+		for (列 = 0;;)
+		{
+			语句文本 << 表名 << '.' << 所有列名[列] << "=_MATLAB_临时表." << 所有列名[列];
+			if (++列 >= 列数)
+				break;
+			语句文本 << ',';
+		}
+	语句->execute(语句文本.str());
 }
